6200987239b56a3e7434472be370c9037c51b8d7
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2016-01-15 09:28:58 -0700

    some more

--- bool-parser.rkt
+++ bool-parser.rkt
@@ -51,17 +51,17 @@
      ((or-expr)
       $1))
     (or-expr ((or-expr ∨ and-expr)
-               `(∨ ,$1 ,$3))
-              ((and-expr)
-               $1))
+              `(∨ ,$1 ,$3))
+             ((and-expr)
+              $1))
     (and-expr ((and-expr ∧ not-expr)
                `(∧ ,$1 ,$3))
               ((not-expr)
                $1))
     (not-expr ((¬ not-expr)
-                `(¬ ,$2))
+               `(¬ ,$2))
               ((atom)
-                $1))
+               $1))
     (atom ((VAR)
            `(Var ,(string->symbol $1)))
           ((⊤)
@@ -71,37 +71,71 @@
           ((OPAREN bidirect-expr CPAREN)
            $2))
     )))
-(define (remove-xor expr)
+
+(define (eliminate-xor expr)
   (match expr
     [`(⊕ ,a ,b)
      ;=>
-     (let [(nxa (remove-xor a))
-           (nxb (remove-xor b))]
-       `(∨ (∧ ,nxa ,nxb) (∧ (¬ ,nxa) (¬ ,nxb))))]
-    [`(,op ,a ,b)  `(,op ,(remove-xor a) ,(remove-xor b))]
+     (let [(nxa (eliminate-xor a))
+           (nxb (eliminate-xor b))]
+       `(∨ (∧ (¬ ,nxa) ,nxb) (∧ ,nxa (¬ ,nxb))))]
+    [`(,op ,a ,b)  `(,op ,(eliminate-xor a) ,(eliminate-xor b))]
     [`(Var ,x) expr]
-    [`(,op ,a) `(,op ,(remove-xor a))]))
+    [`(,op ,a) `(,op ,(eliminate-xor a))]))
+
+(define (eliminate-bicond expr)
+  (match expr
+    [`(↔ ,a ,b)
+     ;=>
+     (let [(nxa (eliminate-bicond a))
+           (nxb (eliminate-bicond b))]
+       `(∧ (→ ,nxa ,nxb) (→ ,nxb ,nxa)))]
+    [`(,op ,a ,b)  `(,op ,(eliminate-bicond a) ,(eliminate-bicond b))]
+    [`(Var ,x) expr]
+    [`(,op ,a) `(,op ,(eliminate-bicond a))]))
+
+(define (to-cnf expr) ; Returns a list of clauses
+  (match expr
+    [`(↔ ,a ,b)
+     ; =>
+     (list `(→ ,a ,b) `(→ ,b ,a))]
+    [`(→ ,a ,b)
+     ; =>
+     (list `(∨ (¬ ,a) ,b))]
+    [`(∨ ,a ,b)
+     ; => 
+     (list expr)]
+    [`(Var ,x)
+     ; => 
+     (list expr)]))
+
+(define (list->cnf l-expr)
+  (append* (map to-cnf l-expr)))
 
 (define (genvar)
   `(Var ,(gensym)))
 
 (define (tseitin-trans expr)
-  (tseitin-real expr))
+  (let [(transformed (tseitin-real expr))]
+    (cons (first transformed) (second transformed))))
 
 (define (tseitin-real expr)
   (let [(sub-name (genvar))]
     (match expr
-      [`(Var ,x) (list expr (list expr))]
+      [`(Var ,x) (list expr empty)]
       [`(,op ,a ,b)
        ; => 
        (let [(res-a (tseitin-real a))
              (res-b (tseitin-real b))]
-         (cons `(,op ,(first 
-         )]
-      [`(,op ,a)
+         (list sub-name
+               (cons `(↔ ,sub-name (,op ,(first res-a) ,(first res-b)))
+                     (append (second res-a) (second res-b)))))]
+      [`(,op ,a)       
        ; =>
-       (let [(name-a (genvar))]
-         (cons `(,op ,name-a) (tseitin-real a name-a)))]))
+       (let [(res-a (tseitin-real a))]
+         (list sub-name
+               (cons `(↔ ,sub-name (,op ,(first res-a)))
+                     (second res-a))))])))
 
 (define (walk-fix proc expr (old '()))
   (if (equal? old expr)
@@ -111,7 +145,7 @@
 
 (define (lex-this lexer input) (lambda () (lexer input)))
 
-(let ((input (open-input-string "a and b")))
+(let ((input (open-input-string "(a -> c) iff 1")))
   
   
-  (display (tseitin-trans (remove-xor (bool-parser (lex-this bool-lexer input))))))
+  (display (list->cnf (list->cnf (tseitin-trans (eliminate-bicond (eliminate-xor (bool-parser (lex-this bool-lexer input)))))))))
